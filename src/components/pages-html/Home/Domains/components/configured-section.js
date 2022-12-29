ebe762a3f5380bda96081b2af07a0cca769c116a
Wong Lok <wonglok@users.noreply.github.com>
2022-12-29 19:38:03 +0800

    save

--- src/components/pages-html/Home/Domains/components/configured-section.js
+++ src/components/pages-html/Home/Domains/components/configured-section.js
@@ -25,20 +25,20 @@ const ConfiguredSection = ({ domainInfo }) => {
     )
     return (
       <>
-        <div className="flex items-center space-x-3 my-3 px-2 sm:px-10">
+        <div className='flex items-center px-2 my-3 space-x-3 sm:px-10'>
           <svg
-            viewBox="0 0 24 24"
-            width="24"
-            height="24"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            shapeRendering="geometricPrecision"
+            viewBox='0 0 24 24'
+            width='24'
+            height='24'
+            strokeWidth='1.5'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            shapeRendering='geometricPrecision'
           >
-            <circle cx="12" cy="12" r="10" fill="#d32f2f" />
+            <circle cx='12' cy='12' r='10' fill='#d32f2f' />
             <>
-              <path d="M15 9l-6 6" stroke="white" />
-              <path d="M9 9l6 6" stroke="white" />
+              <path d='M15 9l-6 6' stroke='white' />
+              <path d='M9 9l6 6' stroke='white' />
             </>
           </svg>
           <p className={`text-red-700 font-medium text-sm`}>
@@ -46,10 +46,10 @@ const ConfiguredSection = ({ domainInfo }) => {
           </p>
         </div>
 
-        <div className="w-full border-t border-gray-100 mt-5 mb-8" />
+        <div className='w-full mt-5 mb-8 border-t border-gray-100' />
 
-        <div className="px-2 sm:px-10">
-          <div className="flex justify-start space-x-4">
+        <div className='px-2 sm:px-10'>
+          <div className='flex justify-start space-x-4'>
             <div
               onClick={() => setRecordType('CNAME')}
               className={`${
@@ -61,19 +61,19 @@ const ConfiguredSection = ({ domainInfo }) => {
               Verify Domain Ownership
             </div>
           </div>
-          <div className="my-3 text-left">
-            <p className="my-5 text-sm">
+          <div className='my-3 text-left'>
+            <p className='my-5 text-sm'>
               Please set the following TXT record on {domainInfo.apexName} to
               prove ownership of {domainInfo.name}:
             </p>
-            <div className="flex justify-start items-start space-x-10 bg-gray-50 p-2 rounded-md">
+            <div className='flex items-start justify-start p-2 space-x-10 bg-gray-50 rounded-md'>
               <div>
-                <p className="text-sm font-bold">Type</p>
-                <p className="text-sm font-mono mt-2">{txtVerification.type}</p>
+                <p className='text-sm font-bold'>Type</p>
+                <p className='mt-2 font-mono text-sm'>{txtVerification.type}</p>
               </div>
               <div>
-                <p className="text-sm font-bold">Name</p>
-                <p className="text-sm font-mono mt-2">
+                <p className='text-sm font-bold'>Name</p>
+                <p className='mt-2 font-mono text-sm'>
                   {txtVerification.domain.slice(
                     0,
                     txtVerification.domain.length -
@@ -83,14 +83,14 @@ const ConfiguredSection = ({ domainInfo }) => {
                 </p>
               </div>
               <div>
-                <p className="text-sm font-bold">Value</p>
-                <p className="text-sm font-mono mt-2">
-                  <span className="text-ellipsis">{txtVerification.value}</span>
+                <p className='text-sm font-bold'>Value</p>
+                <p className='mt-2 font-mono text-sm'>
+                  <span className='text-ellipsis'>{txtVerification.value}</span>
                 </p>
               </div>
             </div>
             {getVerificationError(domainInfo.verificationResponse) && (
-              <p className="my-5 text-sm text-red-700">
+              <p className='my-5 text-sm text-red-700'>
                 {getVerificationError(domainInfo.verificationResponse)}
               </p>
             )}
@@ -102,34 +102,34 @@ const ConfiguredSection = ({ domainInfo }) => {
 
   return (
     <>
-      <div className="flex items-center space-x-3 my-3 px-2 sm:px-10">
+      <div className='flex items-center px-2 my-3 space-x-3 sm:px-10'>
         <svg
-          viewBox="0 0 24 24"
-          width="24"
-          height="24"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          shapeRendering="geometricPrecision"
+          viewBox='0 0 24 24'
+          width='24'
+          height='24'
+          strokeWidth='1.5'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          shapeRendering='geometricPrecision'
         >
           <circle
-            cx="12"
-            cy="12"
-            r="10"
+            cx='12'
+            cy='12'
+            r='10'
             fill={domainInfo.configured ? '#1976d2' : '#d32f2f'}
           />
           {domainInfo.configured ? (
             <>
               <path
-                d="M8 11.8571L10.5 14.3572L15.8572 9"
-                fill="none"
-                stroke="white"
+                d='M8 11.8571L10.5 14.3572L15.8572 9'
+                fill='none'
+                stroke='white'
               />
             </>
           ) : (
             <>
-              <path d="M15 9l-6 6" stroke="white" />
-              <path d="M9 9l6 6" stroke="white" />
+              <path d='M15 9l-6 6' stroke='white' />
+              <path d='M9 9l6 6' stroke='white' />
             </>
           )}
         </svg>
@@ -146,10 +146,10 @@ const ConfiguredSection = ({ domainInfo }) => {
 
       {!domainInfo.configured && (
         <>
-          <div className="w-full border-t border-gray-100 mt-5 mb-8" />
+          <div className='w-full mt-5 mb-8 border-t border-gray-100' />
 
-          <div className="px-2 sm:px-10">
-            <div className="flex justify-start space-x-4">
+          <div className='px-2 sm:px-10'>
+            <div className='flex justify-start space-x-4'>
               <button
                 onClick={() => setRecordType('CNAME')}
                 className={`${
@@ -171,24 +171,24 @@ const ConfiguredSection = ({ domainInfo }) => {
                 A Record (apex domain)
               </button>
             </div>
-            <div className="my-3 text-left">
-              <p className="my-5 text-sm">
+            <div className='my-3 text-left'>
+              <p className='my-5 text-sm'>
                 Set the following record on your DNS provider to continue:
               </p>
-              <div className="flex justify-start items-center space-x-10 bg-gray-50 p-2 rounded-md">
+              <div className='flex items-center justify-start p-2 space-x-10 bg-gray-50 rounded-md'>
                 <div>
-                  <p className="text-sm font-bold">Type</p>
-                  <p className="text-sm font-mono mt-2">{recordType}</p>
+                  <p className='text-sm font-bold'>Type</p>
+                  <p className='mt-2 font-mono text-sm'>{recordType}</p>
                 </div>
                 <div>
-                  <p className="text-sm font-bold">Name</p>
-                  <p className="text-sm font-mono mt-2">
+                  <p className='text-sm font-bold'>Name</p>
+                  <p className='mt-2 font-mono text-sm'>
                     {recordType == 'CNAME' ? 'www' : '@'}
                   </p>
                 </div>
                 <div>
-                  <p className="text-sm font-bold">Value</p>
-                  <p className="text-sm font-mono mt-2">
+                  <p className='text-sm font-bold'>Value</p>
+                  <p className='mt-2 font-mono text-sm'>
                     {recordType == 'CNAME'
                       ? `cname.platformize.co`
                       : `76.76.21.21`}
--- src/components/pages-html/Home/Domains/components/domain-card-placeholder.js
+++ src/components/pages-html/Home/Domains/components/domain-card-placeholder.js
@@ -3,19 +3,19 @@ import ConfiguredSectionPlaceholder from './configured-section-placeholder'
 
 const DomainCardPlaceholder = () => {
   return (
-    <div className="w-full mt-10 sm:shadow-md border-y sm:border border-black sm:border-gray-50 sm:rounded-lg py-10">
-      <div className="flex justify-between space-x-4 px-2 sm:px-10">
-        <div className="h-7 w-36 bg-gray-300 rounded-md animate-pulse" />
-        <div className="flex space-x-3">
+    <div className='w-full py-10 mt-10 border-black sm:shadow-md border-y sm:border sm:border-gray-50 sm:rounded-lg'>
+      <div className='flex justify-between px-2 space-x-4 sm:px-10'>
+        <div className='bg-gray-300 h-7 w-36 rounded-md animate-pulse' />
+        <div className='flex space-x-3'>
           <button
             disabled={true}
-            className="cursor-not-allowed bg-gray-100 text-gray-500 border-gray-200 py-1.5 w-24 text-sm border-solid border rounded-md focus:outline-none transition-all ease-in-out duration-150"
+            className='w-24 text-sm text-gray-500 bg-gray-100 border border-gray-200 border-solid cursor-not-allowed py-1.5 rounded-md focus:outline-none transition-all ease-in-out duration-150'
           >
             <LoadingDots />
           </button>
           <button
             disabled={true}
-            className="bg-red-500 text-white border-red-500 hover:text-red-500 hover:bg-white py-1.5 w-24 text-sm border-solid border rounded-md focus:outline-none transition-all ease-in-out duration-150"
+            className='w-24 text-sm text-white bg-red-500 border border-red-500 border-solid hover:text-red-500 hover:bg-white py-1.5 rounded-md focus:outline-none transition-all ease-in-out duration-150'
           ></button>
         </div>
       </div>
