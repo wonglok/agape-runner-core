10afe9a52ecb957073ede2f4d34de807a8546810
Wong Lok <wonglok@users.noreply.github.com>
2023-01-01 07:48:26 +0800

    do u see

--- src/components/pages-html/SiteCreation/CreateSite.jsx
+++ src/components/pages-html/SiteCreation/CreateSite.jsx
@@ -1,18 +1,24 @@
+import { useRef } from 'react'
 import { GuideHeader } from './GuideHeader'
 import { GuideNav } from './GuideNav'
 
 export function CreateSite() {
+  let siteIDRef = useRef()
   return (
     <div>
       <GuideNav></GuideNav>
       <div className='p-4'>
-        <h1 className='text-xl'>Give your site a name!</h1>
-        <div className=''>
+        <h1 className='text-2xl'>Give your metaverse a name!</h1>
+        <div className='p-2 pl-0'>
           https://
-          <input className='inline border-b border-gray-500 appearance-none  w-36'></input>
+          <input
+            ref={siteIDRef}
+            className='inline w-24 border-b border-gray-500 appearance-none'
+          ></input>
           .at.agape.town
         </div>
       </div>
+
       {/* <GuideHeader active={0} scrollLeft={0}></GuideHeader> */}
     </div>
   )
