b638a5a047ee346193e79e323b2d9fe9deb92789
Wong Lok <wonglok@users.noreply.github.com>
2023-01-04 20:15:19 +0800

    yo

--- src/helpers/DynamicPage.js
+++ src/helpers/DynamicPage.js
@@ -133,132 +133,138 @@ DynamicPage.layout = 'Multiverse'
 export const getServerSidePropsForDynamicPage =
   ({ isIndex }) =>
   async (context) => {
-    //
     let siteOID = null
-
-    // preview route
-    if (context?.params?.siteOID) {
-      siteOID = context?.params?.siteOID
-    }
-
-    let host = context?.req?.headers?.host
-
-    // host = 'kam.agape.land'
-
-    if (typeof host === 'string' && host !== '') {
-      if (host.includes('.at.agape.town')) {
-        //
-        let slug = host.replace('.at.agape.town', '') || ''
-
-        let response = await fetch(
-          `${UserEndPoints[process.env.NODE_ENV]}/seo-subdomain-site`,
-          {
-            body: JSON.stringify({
-              //
-              slug: slug,
-            }),
-            method: 'POST',
-            mode: 'cors',
-          }
-        )
-        let result = await response.json()
-        //
-        let list = result?.list
-        if (list) {
-          let first = list[0]
-
-          if (first) {
-            siteOID = first.oid
-          }
-        }
-      } else {
-        //
-
-        let slug = host + '' || ''
-
-        // console.log(slug)
-
-        let response = await fetch(
-          `${UserEndPoints[process.env.NODE_ENV]}/seo-userdomain-site`,
-          {
-            body: JSON.stringify({
-              //
-              slug: slug,
-            }),
-            method: 'POST',
-            mode: 'cors',
-          }
-        )
-        let result = await response.json()
-        //
-
-        let list = result?.list
-        if (list) {
-          let first = list[0]
-
-          if (first) {
-            siteOID = first.siteID
-          }
-        }
-      }
-    }
-
     let pageData = {}
 
-    if (siteOID) {
-      if (isIndex) {
-        let response = await fetch(
-          `${UserEndPoints[process.env.NODE_ENV]}/seo-site-page`,
-          {
-            body: JSON.stringify({
-              //
-              siteID: siteOID,
-              slug: '',
-            }),
-            method: 'POST',
-            mode: 'cors',
+    try {
+      // preview route
+      if (context?.params?.siteOID) {
+        siteOID = context?.params?.siteOID
+      }
+
+      let host = context?.req?.headers?.host
+
+      // host = 'kam.agape.land'
+
+      if (typeof host === 'string' && host !== '') {
+        if (host.includes('.at.agape.town')) {
+          //
+          let slug = host.replace('.at.agape.town', '') || ''
+
+          let response = await fetch(
+            `${UserEndPoints[process.env.NODE_ENV]}/seo-subdomain-site`,
+            {
+              body: JSON.stringify({
+                //
+                slug: slug,
+              }),
+              method: 'POST',
+              mode: 'cors',
+            }
+          )
+          let result = await response.json()
+          //
+          let list = result?.list
+          if (list) {
+            let first = list[0]
+
+            if (first) {
+              siteOID = first.oid
+            }
           }
-        )
-        let result = await response.json()
+        } else {
+          //
 
-        let list = result?.list
-        if (list) {
-          let first = list[0]
+          let slug = host + '' || ''
 
-          if (first) {
-            pageData = first
-            // console.log(pageData)
-          }
-        }
-      } else {
-        // console.log(slug)
+          // console.log(slug)
 
-        let response = await fetch(
-          `${UserEndPoints[process.env.NODE_ENV]}/seo-site-page`,
-          {
-            body: JSON.stringify({
-              //
-              siteID: siteOID,
-              slug: context?.params?.slug || '',
-            }),
-            method: 'POST',
-            mode: 'cors',
-          }
-        )
-        let result = await response.json()
+          let response = await fetch(
+            `${UserEndPoints[process.env.NODE_ENV]}/seo-userdomain-site`,
+            {
+              body: JSON.stringify({
+                //
+                slug: slug,
+              }),
+              method: 'POST',
+              mode: 'cors',
+            }
+          )
+          let result = await response.json()
+          //
 
-        let list = result?.list
-        if (list) {
-          let first = list[0]
+          let list = result?.list
+          if (list) {
+            let first = list[0]
 
-          if (first) {
-            pageData = first
+            if (first) {
+              siteOID = first.siteID
+            }
           }
         }
       }
+
+      if (siteOID) {
+        if (isIndex) {
+          let response = await fetch(
+            `${UserEndPoints[process.env.NODE_ENV]}/seo-site-page`,
+            {
+              body: JSON.stringify({
+                //
+                siteID: siteOID,
+                slug: '',
+              }),
+              method: 'POST',
+              mode: 'cors',
+            }
+          )
+          let result = await response.json()
+
+          let list = result?.list
+          if (list) {
+            let first = list[0]
+
+            if (first) {
+              pageData = first
+              // console.log(pageData)
+            }
+          }
+        } else {
+          // console.log(slug)
+
+          let response = await fetch(
+            `${UserEndPoints[process.env.NODE_ENV]}/seo-site-page`,
+            {
+              body: JSON.stringify({
+                //
+                siteID: siteOID,
+                slug: context?.params?.slug || '',
+              }),
+              method: 'POST',
+              mode: 'cors',
+            }
+          )
+          let result = await response.json()
+
+          let list = result?.list
+          if (list) {
+            let first = list[0]
+
+            if (first) {
+              pageData = first
+            }
+          }
+        }
+        //
+      }
+
+      //
+    } catch (e) {
+      //
+      console.error(e)
+    } finally {
       //
     }
-
     // //
     // let domainMapping = false
 
