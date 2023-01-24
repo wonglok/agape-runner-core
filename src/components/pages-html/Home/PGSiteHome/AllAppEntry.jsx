0db5c0473b6f8b3f536681440cfc0a6d2ec06bac
Wong Lok <wonglok@users.noreply.github.com>
2023-01-24 09:40:18 +0800

    all app

--- src/components/pages-html/Home/PGSiteHome/AllAppEntry.jsx
+++ src/components/pages-html/Home/PGSiteHome/AllAppEntry.jsx
@@ -23,22 +23,20 @@ export function AllAppEntry() {
     <div className='flex-none w-full max-w-full px-4 mt-4 mb-6'>
       <div className='relative flex flex-col min-w-0 mx-2 break-words bg-white border shadow-inner border-slate-400 shadow-slate-200 shadow-soft-xl rounded-2xl bg-clip-border'>
         <div className='p-4 pb-0 mb-0 rounded-t-2xl'>
-          <h6 className=' mb-3 text-xl'>MetaOS Page</h6>
+          <h6 className=' mb-3 text-xl'>MetaOS Pages</h6>
 
           <div className='inline-block w-full mb-3 lg:w-1/3'>
             <Input
               onInput={(ev) => {
                 setQuery(ev.target.value)
               }}
-              placeholder='search and filter hashtags'
+              placeholder='Search pages and filter hashtags'
             ></Input>
           </div>
 
           {result.map((it) => {
             return <OneEntry key={it.oid} oid={it.oid}></OneEntry>
           })}
-
-          {/*  */}
 
           {cs.appEntry.length === 0 && (
             <>
@@ -71,7 +69,7 @@ function OneEntry({ oid }) {
           }}
           className='inline-block p-2 px-4 mr-2 text-white bg-blue-500 rounded-xl'
         >
-          Edit App Package
+          Edit
         </button>
 
         <div className='inline-block'>
