39b4d8da3e88fd2cddca9d4c2bf332449cba82b6
Wong Lok <wonglok@users.noreply.github.com>
2023-01-24 17:06:30 +0800

    app entry

--- src/components/pages-html/Home/PGSiteHome/AllAppEntry.jsx
+++ src/components/pages-html/Home/PGSiteHome/AllAppEntry.jsx
@@ -24,7 +24,7 @@ export function AllAppEntry() {
     <div className='flex-none w-full max-w-full px-4 mt-4 mb-6'>
       <div className='relative flex flex-col min-w-0 mx-2 break-words bg-white border shadow-inner border-slate-400 shadow-slate-200 shadow-soft-xl rounded-2xl bg-clip-border'>
         <div className='p-4 pb-0 mb-0 rounded-t-2xl'>
-          <h6 className=' mb-3 text-xl'>WebNative Metaverse OS</h6>
+          <h6 className=' mb-3 text-xl'>Metaverse Pages</h6>
 
           <div className='py-2'>
             <CreateApp />
