d424e6ea950c80af68b7e640d6ed8a17e49ad9b7
Wong Lok <wonglok@users.noreply.github.com>
2023-01-24 20:11:12 +0800

    face

--- src/components/pages-html/Home/PGSiteHome/AllAppContent.jsx
+++ src/components/pages-html/Home/PGSiteHome/AllAppContent.jsx
@@ -21,8 +21,8 @@ export function AllAppContent({}) {
         <div className='flex-none w-full max-w-full px-4 mt-6 mb-4 '>
           <div className='relative flex flex-col min-w-0 mx-2 break-words bg-white border shadow-inner border-slate-400 shadow-slate-200 shadow-soft-xl rounded-2xl bg-clip-border'>
             <div className='p-4 pb-4 mb-0 rounded-t-2xl'>
-              <h6 className=' mb-3 text-2xl'>{`Let's Install an App`}</h6>
-              <h6 className=' mb-3 text-lg'>/{appEntryOne.slug}</h6>
+              <h6 className=' mb-3 text-2xl'>Page /{appEntryOne.slug}</h6>
+              <h6 className=' mb-3 text-lg'>{`Let's Install an App`}</h6>
 
               <div className='flex flex-wrap mb-4'>
                 <button
