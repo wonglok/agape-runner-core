be13167b6567f871a7c9e9c3aeb27574abfe8b41
Wong Lok <wonglok@users.noreply.github.com>
2023-01-25 15:42:50 +0800

    save

--- src/components/pages-html/BUCodeStudio/Implementation/FileTree/FileTree.jsx
+++ src/components/pages-html/BUCodeStudio/Implementation/FileTree/FileTree.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import { AppDev } from '@/aws/AppDev'
 import { getID } from '@/lib/getID'
+import { Button, Tooltip } from 'antd'
 import { useSnapshot } from 'valtio'
 
 export function FileTree() {
@@ -23,26 +24,31 @@ function MyPakcages({ dev }) {
 
   return (
     <>
-      <div className='flex items-center justify-between p-1 pl-2 bg-gray-100'>
+      <div className='flex items-center justify-between pr-1 bg-gray-100 group'>
         <select
           onChange={async (ev) => {
             AppDev.draft.appLoader = ev.target.value
             await AppDev.save({ object: AppDev.draft })
           }}
           defaultValue={AppDev.draft.appLoader}
-          className='w-full h-6 '
+          className='w-full py-2 pl-2 bg-transparent appearance-none focus:outline-none'
         >
           {appPackages.map((ap) => {
             return (
-              <option className='p-1' key={ap.oid} value={ap.packageName}>
+              <option className='' key={ap.oid} value={ap.packageName}>
                 {ap.packageName}
               </option>
             )
           })}
         </select>
+        <img
+          className='h-6 group-hover:animate-pulse'
+          src={'/code-studio-ui/circle-arrow-left.svg'}
+          alt={'add'}
+        ></img>
       </div>
 
-      <div className='flex items-center justify-between pl-2 bg-gray-100'>
+      <div className='flex items-center justify-between pl-2 bg-gray-100 group'>
         <div>Packages</div>
         <div
           className='px-1 py-1 cursor-auto'
@@ -58,20 +64,17 @@ function MyPakcages({ dev }) {
             } else {
               AppDev.draft.appPackages.push({
                 oid: getID(),
-                packageName: 'content-package',
+                packageName: getID(),
                 modules: [],
               })
             }
 
             await AppDev.save({ object: AppDev.draft })
-
-            //
-            //
           }}
         >
           {/*  */}
           <img
-            className='h-6'
+            className='h-6 group-hover:animate-pulse'
             src={'/code-studio-ui/plus.svg'}
             alt={'add'}
           ></img>
@@ -81,10 +84,32 @@ function MyPakcages({ dev }) {
         return (
           <div
             key={ap.oid}
-            className='flex items-center justify-between w-full h-8'
+            className='flex items-center justify-between w-full h-8 pl-2'
           >
-            <div>{ap.packageName}</div>
-            <div></div>
+            <div className='w-full overflow-hidden'>
+              <Tooltip
+                placement='right'
+                title={
+                  <div>
+                    <input
+                      className='p-2 text-white focus:outline-none  '
+                      style={{ backgroundColor: `transparent` }}
+                      defaultValue={ap.packageName}
+                      onInput={(ev) => {
+                        let item = AppDev.draft.appPackages.find(
+                          (e) => e.oid === ap.oid
+                        )
+                        if (item) {
+                          item.packageName = ev.target.value
+                        }
+                      }}
+                    ></input>
+                  </div>
+                }
+              >
+                <div className=' whitespace-pre'>{ap.packageName}</div>
+              </Tooltip>
+            </div>
           </div>
         )
       })}
