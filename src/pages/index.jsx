9609c66db4c75f9343a7d3047c0db9512c6a32e4
Wong Lok <wonglok@users.noreply.github.com>
2023-01-29 14:19:41 +0800

    save

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,6 +10,13 @@ export default function SlugPage() {
   let [outputs, setOutputs] = useState(false)
 
   useEffect(() => {
+    setOutputs([
+      {
+        fileName: 'index.js',
+        code: "function MyApp () {\n    let { Canvas } = window.R3F;\n    let { Sphere, MeshTransmissionMaterial, Environment, OrbitControls } = window.Drei;\n    let { EffectComposer, Bloom } = window.PostProc;\n \n    return React.createElement(React.Fragment, null\n        , React.createElement(Canvas, null\n            , React.createElement(Sphere, { args: [1.5,32,32],}\n                , React.createElement(MeshTransmissionMaterial, {\n                    flatShading: false,\n                    transmission: 1,\n                    roughness: 0.0,\n                    resolution: 512,\n                    thickness: 3,\n                    ior: 1.3,\n                    metalness: 0.0,\n                    reflectivity: 0.5,}\n                )\n            )\n\n            , React.createElement(Environment, { preset: \"sunset\", background: true,} )\n\n            , React.createElement(EffectComposer, null\n                , React.createElement(Bloom, { luminanceThreshold: 0.2, intensity: 1, mipmapBlur: true,} )\n            )\n\n            , React.createElement(OrbitControls, null )\n        )\n    )\n}\n\nfunction _optionalChain(ops) { let lastAccessLHS = undefined; let value = ops[0]; let i = 1; while (i < ops.length) { const op = ops[i]; const fn = ops[i + 1]; i += 2; if ((op === 'optionalAccess' || op === 'optionalCall') && value == null) { return undefined; } if (op === 'access' || op === 'optionalAccess') { lastAccessLHS = value; value = fn(value); } else if (op === 'call' || op === 'optionalCall') { value = fn((...args) => value.call(lastAccessLHS, ...args)); lastAccessLHS = undefined; } } return value; }\nasync function Page ({ domElement, onClean }) {\n    let [THREE, R3F, Drei, PostProc, XR3] = await Promise.all([\n        window.getThree(),\n        window.getR3F(),\n        window.getDrei(),\n        window.getPost(),\n        window.getXR()\n    ]);\n    window.THREE = THREE;\n    window.R3F = R3F;\n    window.Drei = Drei;\n    window.PostProc = PostProc;\n    window.XR3 = XR3;\n\n    window.appRoot = window.appRoot || ReactDOM.createRoot(domElement);\n    window.appRoot.render(React.createElement(MyApp, null));\n\n    onClean(() => {\n        console.log('unmount');\n        _optionalChain([window, 'optionalAccess', _ => _.appRoot, 'optionalAccess', _2 => _2.unmount, 'call', _3 => _3()]);\n        delete window.appRoot;\n    });\n}\n\nexport { Page as default };\n",
+      },
+    ])
+
     if (query && query.slug) {
       //
       let slugString = query.slug.join('/')
@@ -27,5 +34,5 @@ export default function SlugPage() {
     }
   }, [query])
 
-  return <>{outputs && <BUCodeRunner outputs={outputs}></BUCodeRunner>}</>
+  return <>{outputs && <BUCodeRunner outputsJSON={outputs}></BUCodeRunner>}</>
 }
